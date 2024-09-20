b0a289ff6f2249db5f2760efdb92df83118bb46f
ellen samanta <ellen.samanta@outlook.com>
2024-09-20 11:40:05 -0300

    feat: criando estrutura backend do projeto

---
+++ backend_repositorios/src/main/java/com/repositorios/backend_repositorios/BackendRepositoriosApplication.java
@@ -0,0 +1,13 @@
+package com.repositorios.backend_repositorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRepositoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRepositoriosApplication.class, args);
+	}
+
+}
---
+++ backend_repositorios/src/test/java/com/repositorios/backend_repositorios/BackendRepositoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repositorios.backend_repositorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRepositoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
