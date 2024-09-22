f8cfc2a8f2206f208999769ac20a9ad4bc9990d4
ellen samanta <ellen.samanta@outlook.com>
2024-09-22 12:14:11 -0300

    feat: criando escopo da pagina

---
+++ frontend/flix-repositories/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function PageHome(props){
+return(
+    <div className="PagesHome">
+        <div className="Layout_perfil">
+
+        </div>
+
+        <div className="list_repositories">
+
+        </div>
+
+    </div>
+)
+}
