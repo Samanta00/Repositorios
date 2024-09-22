ecaa74d431f0897e8913fdd14a7c0f620361447b
ellen samanta <ellen.samanta@outlook.com>
2024-09-22 15:42:47 -0300

    feat: criando arquivo para layout do perfil

---
+++ frontend/flix-repositories/src/pages/Layout_perfil/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function LayoutPerfil(props){
+return(
+    <div className="PagesHome">
+        <div className="Layout_perfil">
+            <h4>testeeeeeeeeeeeeeeeee</h4>
+        </div>
+
+    </div>
+)
+}
+
+export default LayoutPerfil;
