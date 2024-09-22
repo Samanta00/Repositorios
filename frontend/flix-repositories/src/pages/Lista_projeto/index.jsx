560fc975df908ac903494ee72f78dd5f75516a28
ellen samanta <ellen.samanta@outlook.com>
2024-09-22 15:43:08 -0300

    feat: criando arquivo para listar os repositorios

---
+++ frontend/flix-repositories/src/pages/Lista_projeto/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function ListaProjeto(props){
+return(
+    <div className="Lista_Projeto">
+        <div className="list_repositories">
+            <h1>Teste123</h1>
+            {/* <h3>{props.name_project}</h3>
+            <h5>{props.descricao}</h5> */}
+
+        </div>
+    </div>
+)
+}
+
+export default ListaProjeto;
